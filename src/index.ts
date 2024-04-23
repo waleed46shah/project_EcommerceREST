5714ae8b2e8c0c8dce4dafebc5206adf1de29372
waleed46shah <waleed46shah@gmail.com>
2024-04-23 19:36:03 +0500

    Add get and update user route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/connectDB";
 import authRoute from "./routes/auth";
+import userRoute from "./routes/user";
 import { errorHandler } from "./middleware/error";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -10,7 +11,9 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 app.use(errorHandler);
 
---
+++ src/routes/user.ts
@@ -0,0 +1,52 @@
+import express, { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User";
+import { CustomError } from "../middleware/error";
+
+const router = express.Router();
+
+// UPDATE USER ROUTE
+router.put(
+  "/update/:userId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hashSync(req.body.password, salt);
+        req.body.password = hashedPassword;
+      }
+
+      const user = await User.findById(req.params.userId);
+      if (!user) {
+        throw new CustomError(404, "User not found!");
+      }
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $set: req.body },
+        { new: true }
+      );
+
+      res.status(200).json(updateUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//GET USER ROUTE
+router.get(
+  "/:userId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      if (!user) {
+        throw new CustomError(404, "User not found!");
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default router;
