228db9cac457c889e3ded92f588145357c2cce30
waleed46shah <waleed46shah@gmail.com>
2024-04-24 16:15:33 +0500

    search product rote

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -16,4 +16,24 @@ router.post(
     }
   }
 );
+
+// SEARCH PRODUCT ROUTE
+router.get(
+  "/search/:query",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const query = req.params.query;
+    try {
+      const products = await Product.find({
+        $or: [
+          { name: { $regex: new RegExp(query, "i") } },
+          { category: { $regex: new RegExp(query, "i") } },
+        ],
+      });
+      res.status(200).json(products);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default router;
