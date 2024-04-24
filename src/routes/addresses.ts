cb7b447874e74c6f93639c716f1b93cd83c58594
waleed46shah <waleed46shah@gmail.com>
2024-04-24 19:13:34 +0500

    Complete Address CRUD

--- src/routes/addresses.ts
+++ src/routes/addresses.ts
@@ -1,9 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
 import Address from "../models/Address";
+import { CustomError } from "../middleware/error";
 
 const router = express.Router();
 
-// CREATE ADDRESS
+// CREATE ADDRESS ROUTE
 router.post(
   "/create",
   async (req: Request, res: Response, next: NextFunction) => {
@@ -13,6 +14,49 @@ router.post(
       res.status(201).json(savedAddress);
     } catch (error) {
       console.log("err");
+      next(error);
+    }
+  }
+);
+
+// UPDATE ADDRESS ROUTE
+router.put(
+  "/update/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const updatedAddress = await Address.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedAddress);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// DELETE ADDRESS ROUTE
+router.delete(
+  "/delete/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await Address.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "Address deleted successfully!" });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//GET USER ADDRESSES
+router.get(
+  "/:userId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const addresses = await Address.find({ user: req.params.userId });
+      res.status(200).json(addresses);
+    } catch (error) {
       next(error);
     }
   }
