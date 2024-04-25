c8afb71901e12c2491c345b90c0f781ced6d60a9
waleed46shah <waleed46shah@gmail.com>
2024-04-25 15:09:05 +0500

    Create add and remove cart route

--- src/routes/carts.ts
+++ src/routes/carts.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import Cart from "../models/Cart";
+import { CustomError } from "../middleware/error";
 
 const router = express.Router();
 
@@ -28,5 +29,56 @@ router.post("/add", async (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 });
+
+//REMOVE ITEM FROM CART ROUTE (REMOVE -1 FROM QUANTITY)
+router.post(
+  "/remove",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { productId, userId } = req.body;
+      const cart = await Cart.findOne({ user: userId });
+      if (!cart) {
+        throw new CustomError(404, "Cart is empty!");
+      }
+
+      const productIndex = cart.products.findIndex(
+        (item) => item.product.toString() === productId
+      );
+      if (productIndex !== -1) {
+        if (cart.products[productIndex].quantity > 1) {
+          cart.products[productIndex].quantity -= 1;
+        } else {
+          cart.products.splice(productIndex, 1);
+        }
+
+        await cart.save();
+        res
+          .status(200)
+          .json({ message: "Item removed from cart successfully!" });
+      } else {
+        throw new CustomError(404, "Product not found in cart!");
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//GET USER CART ROUTE
+router.get(
+  "/:userId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.params.userId;
+      const cart = await Cart.findOne({ user: userId });
+      if (!cart) {
+        throw new CustomError(404, "Cart is empty!");
+      }
+      res.status(200).json(cart);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
